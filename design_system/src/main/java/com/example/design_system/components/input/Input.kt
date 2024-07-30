cff2698899ca2d30e07fd561214f41a5d84ac1de
sandghuladze <sandghuladze@bog.ge>
2024-07-30 18:44:58 +0400

    added input on chat screen

--- design_system/src/main/java/com/example/design_system/components/input/Input.kt
+++ design_system/src/main/java/com/example/design_system/components/input/Input.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import android.text.InputType
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
+import androidx.core.view.children
 import androidx.core.view.setPadding
+import androidx.core.view.updateLayoutParams
 import androidx.core.widget.addTextChangedListener
 import com.example.design_system.R
+import com.example.design_system.components.empty.EmptyView
 import com.example.design_system.components.input.validator.InputValidation
 import com.example.design_system.components.input.validator.InputValidator
 import com.example.design_system.databinding.LayoutForoomInputBinding
-import com.example.shared.extension.INVALID_RESOURCE
+import com.example.shared.extension.INVALID_VALUE
+import com.example.shared.extension.checkValue
 import com.example.shared.extension.hide
-import com.example.shared.extension.ifValidResource
+import com.example.shared.extension.ifValidValue
 import com.example.shared.extension.show
 
 class Input @JvmOverloads constructor(
@@ -32,25 +37,60 @@ class Input @JvmOverloads constructor(
     val editText get() = binding.inputEditText
     var shouldClearDescriptionOnTextChange = false
 
+    var startView: View? = null
+        set(value) {
+            handleNewStartView(value)
+            field = value
+        }
+
+    var endView: View? = null
+        set(value) {
+            handleNewEndView(value)
+            field = value
+        }
+
     init {
         resources.obtainAttributes(attrs, R.styleable.Input).apply {
             descriptionType =
                 DescriptionType.fromValue(getInt(R.styleable.Input_descriptionType, -1))
             description = getString(R.styleable.Input_description)
 
+            getDrawable(R.styleable.Input_inputBackground)?.let { drawable ->
+                binding.contentLinearLayout.background = drawable
+            }
+
+            getColor(R.styleable.Input_inputBackgroundTint, INVALID_VALUE).ifValidValue { value ->
+                binding.contentLinearLayout.background.setTint(value)
+            }
+
             with(editText) {
                 hint = getString(R.styleable.Input_hint)
                 inputType = getInt(R.styleable.Input_android_inputType, InputType.TYPE_CLASS_TEXT)
 
-                val drawableTint = getColor(R.styleable.Input_drawableTint, INVALID_RESOURCE)
+                val start = getDimensionPixelSize(
+                    R.styleable.Input_inputPaddingStart,
+                    INVALID_VALUE
+                )
+
+                val end = getDimensionPixelSize(
+                    R.styleable.Input_inputPaddingEnd,
+                    INVALID_VALUE
+                )
+
+                this.updateLayoutParams<MarginLayoutParams> {
+                    leftMargin = checkValue(start, marginStart)
+                    rightMargin = checkValue(end, marginEnd)
+                }
+
+                val drawableTint = getColor(R.styleable.Input_drawableTint, INVALID_VALUE)
 
                 setCompoundDrawablesRelativeWithIntrinsicBounds(
                     getDrawable(R.styleable.Input_android_drawableStart).apply {
-                        drawableTint.ifValidResource { this?.setTint(drawableTint) }
+                        drawableTint.ifValidValue { this?.setTint(drawableTint) }
                     },
                     null,
                     getDrawable(R.styleable.Input_android_drawableEnd).apply {
-                        drawableTint.ifValidResource { this?.setTint(drawableTint) }
+                        drawableTint.ifValidValue { this?.setTint(drawableTint) }
                     },
                     null
                 )
@@ -77,6 +117,25 @@ class Input @JvmOverloads constructor(
         }.recycle()
 
         handleTextChange()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        val views = children.toMutableList()
+        views.removeFirst()
+
+        views.firstOrNull()?.let { v ->
+            removeView(v)
+            startView = v
+            views.removeFirst()
+        }
+
+        views.firstOrNull()?.let { v ->
+            removeView(v)
+            endView = v
+            views.removeFirst()
+        }
     }
 
     fun setInputDescription(
@@ -136,8 +195,31 @@ class Input @JvmOverloads constructor(
         }
     }
 
+    private fun handleNewStartView(view: View?) {
+        if (startView != null) {
+            binding.contentLinearLayout.removeViewAt(START_VIEW_INDEX)
+        }
+
+        if (view != null && view !is EmptyView) {
+            binding.contentLinearLayout.addView(view, START_VIEW_INDEX)
+        }
+    }
+
+    private fun handleNewEndView(view: View?) {
+        if (endView != null) {
+            binding.contentLinearLayout.removeViewAt(END_VIEW_INDEX)
+        }
+
+        if (view != null && view !is EmptyView) {
+            binding.contentLinearLayout.addView(view)
+        }
+    }
+
     companion object {
         private const val ZERO_PADDING = 0
+
+        private const val START_VIEW_INDEX = 0
+        private const val END_VIEW_INDEX = 2
     }
 
     enum class DescriptionType(val value: Int) {
--- shared/src/main/java/com/example/shared/extension/NumberExtensions.kt
+++ shared/src/main/java/com/example/shared/extension/NumberExtensions.kt
@@ -2,7 +2,11 @@ package com.example.shared.extension
 
 fun Int.isEven() = this % 2 == 0
 
-const val INVALID_RESOURCE = -1
-fun Int.ifValidResource(block: (Int)-> Unit) {
-    if (!equals(INVALID_RESOURCE)) block(this)
+const val INVALID_VALUE = -1
+fun Int.ifValidValue(block: (Int)-> Unit) {
+    if (!equals(INVALID_VALUE)) block(this)
+}
+
+fun checkValue(value: Int, default: Int): Int {
+    return if (value != INVALID_VALUE) value else default
 }
