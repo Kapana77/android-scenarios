629f5a12ba9da34949ef7bc0a2856958db3085c1
lmosakhlishvili <lmosakhlishvili@tbcbank.com.ge>
2025-07-11 14:38:25 +0400

    update configs

--- app/src/androidTest/java/com/example/foroom/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/foroom/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.foroom
 
+import android.view.View
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.alternator.foroom.R
+import com.example.foroom.Helper.tap
+import com.example.foroom.presentation.ui.activity.ForoomActivity
+import com.example.foroom.presentation.ui.activity.ForoomActivityViewModel
+import org.hamcrest.Matcher
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +24,9 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activity = ActivityScenarioRule(ForoomActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
